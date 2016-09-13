c073dd24f4e99627e44491a2f87c96dd2e3e509a
Elizabeth Sweeney <emsweene@jhsph.edu>
2016-09-13 17:01:06 -0500

    added mris_convert

---
+++ R/mris_convert.R
@@ -0,0 +1,39 @@
+#' @title Use Freesurfers MRIs Converter 
+#' @description This function call  \code{mris_convert}, a general conversion program for converting between cortical surface file formats 
+#' @param curvinfilepath (character) input filename for curve
+#' @param origfilepath (character) file path for origin file
+#' @param outfile (character) output file path 
+#' @return Result of \code{system} command
+#' @export
+#' @examples 
+#' if (have_fs()) {
+#'  mris_convert(curvinfilepath = 'lh.thickness', origfilepath = 'lh.white', outfile = 'lh.thickness.asc')  
+#' } 
+
+mris_convert = function(
+  curvinfilepath, 
+  origfilepath,
+  outfile = NULL,
+  ext = '.asc'){
+  
+  ######################################################    
+  # Making output file if not specified
+  ######################################################      
+  if (is.null(outfile)) {
+    outfile = tempfile(fileext = ext)
+  }
+   
+  cmd <- paste('mris_convert -c', curvinfilepath, origfilepath, outfile, sep = ' ')
+  cmd <- paste0(get_fs(), cmd)
+  res = system(cmd)
+}
+
+
+#' @title Help file for Freesurfers MRIs Converter 
+#' @description This calls Freesurfer's \code{mris_convert} help 
+#'
+#' @return Result of \code{fs_help}
+#' @export
+mri_convert.help = function(){
+  fs_help(func_name = "mris_convert")
+}
