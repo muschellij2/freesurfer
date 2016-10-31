83796e43f5570112d73524f830463e829a2aa6cb
muschellij2 <muschellij2@gmail.com>
2016-10-30 22:51:31 -0400

    Added readmnc.

---
+++ R/readmnc.R
@@ -0,0 +1,22 @@
+#' @title Read MNC File
+#' @description This function calls \code{mnc2nii} 
+#' to convert MNC files to NIfTI, then reads it in using 
+#' \code{\link{readnii}}
+#' @param file (character) input filename
+#' @return Object of class \code{\link{nifti}}
+#' @export
+readmnc = function(file){
+  outfile = tempfile(fileext = ".nii.gz")
+  
+  ret = fs_cmd(
+    func = "mnc2nii",
+    file = file,
+    outfile = outfile,
+    retimg = TRUE,
+    frontopts = "",
+    samefile = FALSE,
+    add_ext = FALSE,
+    bin_app = "mni/bin")
+
+  return(ret)
+}
