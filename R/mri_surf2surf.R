c6dc8e437e41df4c07e371b760db9d4a3d3785d6
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2025-05-11 19:52:55 +0200

    add ... to functions calling fs_cmd

--- R/mri_surf2surf.R
+++ R/mri_surf2surf.R
@@ -1,138 +1,137 @@
-#' @title Use Freesurfers \code{mri_surf2surf} function to 
-#' resamples one cortical surface onto another 
-#' @description This function calls Freesurfer \code{mri_surf2surf} to 
-#' resample one cortical surface onto another 
-#' @param subject (character) vector of subject name 
+#' @title Use Freesurfers \code{mri_surf2surf} function to
+#' resamples one cortical surface onto another
+#' @description This function calls Freesurfer \code{mri_surf2surf} to
+#' resample one cortical surface onto another
+#' @param subject (character) vector of subject name
 #' @param outfile (character) output filename
 #' @param hemi (character) hemisphere to run statistics
-#' @param target_subject (character) vector of target subject name 
+#' @param target_subject (character) vector of target subject name
 #' @param src_type (character) source file type, can be curv or paint (w)
 #' @param trg_type (character) target file type, can be curv, paint (w), mgh, or nii
-#' @param sval (character) source file 
+#' @param sval (character) source file
 #' @param subj_dir (character path) if a different subjects directory
 #' is to be used other than \code{SUBJECTS_DIR} from shell, it can be
 #' specified here.  Use with care as if the command fail, it may not reset
 #' the \code{SUBJECTS_DIR} back correctly after the error
 #' @param opts (character) additional options to \code{mri_surf2surf}
 #' @param verbose (logical) print diagnostic messages
+#' @param ... Additional arguments to pass to \code{\link{system}}
 #' @return Name of output file
 #' @export
-#' @examples 
+#' @examples
 #' if (have_fs()) {
-#'    out = mri_surf2surf( 
+#'    out = mri_surf2surf(
 #'    subject = 'bert',
-#'    target_subject = 'fsaverage', 
-#'    trg_type  = 'curv', 
-#'    src_type  = 'curv', 
+#'    target_subject = 'fsaverage',
+#'    trg_type  = 'curv',
+#'    src_type  = 'curv',
 #'    hemi = "rh",
 #'    sval = "thickness")
-#' } 
+#' }
 mri_surf2surf = function(
-  subject = NULL ,
-  target_subject = NULL, 
-  trg_type  = c('curv', 'w', 'mgh', 'nii'), 
-  src_type  = c('curv', 'w'), 
+  subject = NULL,
+  target_subject = NULL,
+  trg_type = c('curv', 'w', 'mgh', 'nii'),
+  src_type = c('curv', 'w'),
   outfile = NULL,
   hemi = c("lh", "rh"),
   sval = c("thickness"),
   subj_dir = NULL,
   opts = "",
-  verbose = TRUE){
-  
+  verbose = TRUE,
+  ...
+) {
   ###########################
   # Making Hemisphere
-  ###########################  
+  ###########################
   hemi = match.arg(hemi)
   xhemi = hemi
   hemi = paste0("--hemi ", hemi)
   args = hemi
   ###########################
   # Making Subject Vector
-  ###########################    
+  ###########################
   subject = paste0("--s ", subject)
   args = c(args, subject)
   ###########################
-  # Making Target Subject 
-  ###########################  
+  # Making Target Subject
+  ###########################
   target_subject = paste0("--trgsubject ", target_subject)
   args = c(args, target_subject)
   ###########################
-  # Making Target Type 
-  ###########################  
+  # Making Target Type
+  ###########################
   trg_type = match.arg(trg_type)
   trg_type = paste0("--trg_type ", trg_type)
   args = c(args, trg_type)
   ###########################
   # Making Source File
-  ###########################  
+  ###########################
   sval = match.arg(sval)
   # ext = paste0(".", sval)
   sval = paste0("--sval ", sval)
   args = c(args, sval)
   ###########################
-  # Making Source Type 
-  ###########################  
+  # Making Source Type
+  ###########################
   src_type = match.arg(src_type)
   src_type = paste0("--src_type ", src_type)
   args = c(args, src_type)
   ###########################
   # Making output file if not specified
-  ###########################    
+  ###########################
   if (is.null(outfile)) {
     outfile = tempfile()
   }
-  args = c(args, paste0("--tval ", outfile))  
-  outfile = file.path(dirname(outfile), 
-                      paste0(xhemi, ".", basename(outfile)))  
-  
+  args = c(args, paste0("--tval ", outfile))
+  outfile = file.path(dirname(outfile), paste0(xhemi, ".", basename(outfile)))
+
   ###########################
   # Adding verbose option
-  ###########################    
+  ###########################
   if (verbose) {
     args = c(args, "--debug")
   }
-  
-
 
   ###########################
   # Making output file if not specified
 
   ###########################
-  # Need ability to have 
+  # Need ability to have
   # non-standard subjects directory
-  ###########################  
+  ###########################
   cmd_pre = ""
   if (!is.null(subj_dir)) {
     orig_subj_dir = Sys.getenv("SUBJECTS_DIR")
     old_reset = sprintf("export SUBJECTS_DIR=%s; ", orig_subj_dir)
     on.exit({
       system(old_reset)
-    })    
+    })
     subj_dir = path.expand(subj_dir)
     cmd_pre = sprintf("export SUBJECTS_DIR=%s; ", subj_dir)
   }
-  
+
   ###########################
   # Add the Subjects DIR Stuff to the command first
-  ###########################  
+  ###########################
   cmd = paste0(get_fs(), "mri_surf2surf")
   cmd = paste0(cmd_pre, cmd)
-  
+
   args = paste(args, collapse = " ")
   cmd = paste(cmd, args)
   cmd = paste(cmd, opts)
-  
-  run_check_fs_cmd(cmd = cmd, outfile = outfile, verbose = verbose)
-  
+
+  run_check_fs_cmd(cmd = cmd, outfile = outfile, verbose = verbose, ...)
+
   return(outfile)
 }
 
 
 #' @title Freesurfers mri_surf2surf Help
-#' @description This calls Freesurfer's \code{mri_surf2surf} help 
+#' @description This calls Freesurfer's \code{mri_surf2surf} help
 #'
 #' @return Result of \code{fs_help}
 #' @export
-mri_surf2surf.help = function(){
+mri_surf2surf.help = function() {
   fs_help(func_name = "mri_surf2surf", help.arg = "--help")
 }
--- R/nii2mnc.R
+++ R/nii2mnc.R
@@ -1,27 +1,30 @@
-#' @title Convert NIfTI to MNC 
-#' @description This function calls \code{nii2mnc} 
-#' to convert NIfTI to MNC files 
+#' @title Convert NIfTI to MNC
+#' @description This function calls \code{nii2mnc}
+#' to convert NIfTI to MNC files
 #' @param file (character) input filename
 #' @param outfile (character) output filename
+#' @param ... Additional arguments to pass to \code{\link{fs_cmd}}
 #' @return Character filename of output
-#' @importFrom tools file_ext 
+#' @importFrom tools file_ext
 #' @importFrom R.utils gzip gunzip
 #' @importFrom neurobase checknii
 #' @export
-#' @examples 
+#' @examples
 #' if (have_fs() && requireNamespace("oro.nifti", quietly = TRUE)) {
-#'    img = oro.nifti::nifti(array(rnorm(5*5*5), dim = c(5,5,5)))  
+#'    img = oro.nifti::nifti(array(rnorm(5*5*5), dim = c(5,5,5)))
 #'    mnc = nii2mnc(img)
 #'    img_file = mnc2nii(mnc)
 #' }
 nii2mnc = function(
-  file, 
-  outfile = NULL){
+  file,
+  outfile = NULL,
+  ...
+) {
   file = neurobase::checknii(file)
   # file = checkimg(file, gzipped = FALSE)
   # ext = file_ext(tolower(file))
   # if (ext %in% "gz") {
-  #   file = R.utils::gunzip(filename = file, 
+  #   file = R.utils::gunzip(filename = file,
   #                          remove = FALSE,
   #                          temporary = TRUE,
   #                          overwrite = TRUE)
@@ -40,7 +43,9 @@ nii2mnc = function(
     retimg = FALSE,
     samefile = FALSE,
     add_ext = FALSE,
-    bin_app = "mni/bin")
+    bin_app = "mni/bin",
+    ...
+  )
   if (!file.exists(outfile)) {
     stop("nii2mnc did not produce outfile specified")
   }
@@ -49,10 +54,10 @@ nii2mnc = function(
 
 
 #' @title Convert NIfTI to MNC Help
-#' @description This calls Freesurfer's \code{mnc2nii} help 
+#' @description This calls Freesurfer's \code{mnc2nii} help
 #'
 #' @return Result of \code{fs_help}
 #' @export
-nii2mnc.help = function(){
+nii2mnc.help = function() {
   fs_help(func_name = "nii2mnc", help.arg = "", bin_app = "mni/bin")
 }
